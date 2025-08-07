dc3c29f875fddc22cf96a055ab26cd9ba281f0ba
Akash Jaiswal <realakashjaiswal@gmail.com>
2025-08-07 15:54:54 +0530

    updating route mount

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api', routes);
+app.use('/', routes);
 
 sequelize.authenticate().then(() => {
   console.log('SQL db authenticated successfully');
